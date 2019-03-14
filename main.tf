dd98fff276cf0bff75887ce67f964769ed00f49c
userhas404d <29389186+userhas404d@users.noreply.github.com>
2019-03-14 11:14:11 -0400

    Further conditionalizes ssh inbound on the runner(s)

--- main.tf
+++ main.tf
@@ -3,25 +3,16 @@
 ################################################################################
 
 locals {
-  callers_ip                  = ["${chomp(data.http.ip.body)}/32"]
-  any_any_cidr_block          = ["0.0.0.0/0"]
-  specified_cidr_blocks       = "${concat(local.callers_ip, var.specified_cidr_blocks)}"
-  cidr_blocks_allowed_inbound = "${split(",", var.allow_all_inbound ? join(",", local.any_any_cidr_block) : join(",", local.specified_cidr_blocks))}"
-  key_pair_not_specified      = "${var.ssh_key_name == ""}"
+  callers_ip             = ["${chomp(data.http.ip.body)}/32"]
+  specified_cidr_blocks  = "${concat(local.callers_ip, var.specified_cidr_blocks)}"
+  ssh_cidrs_specified    = "${length(var.allow_ssh_to_runner_instance_cidr) == 0}"
+  key_pair_not_specified = "${var.ssh_key_name == ""}"
 }
 
 # sg for the instance that calls docker-machine to deploy additional instances
 resource "aws_security_group" "runner" {
   name_prefix = "${var.environment}-security-group"
   vpc_id      = "${var.vpc_id}"
-
-  ingress {
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    cidr_blocks     = ["${local.cidr_blocks_allowed_inbound}"]
-    security_groups = ["${var.allow_ssh_to_runner_instance_sg}"]
-  }
 
   egress {
     from_port   = 0
@@ -31,6 +22,30 @@ resource "aws_security_group" "runner" {
   }
 
   tags = "${local.tags}"
+}
+
+resource "aws_security_group_rule" "runner_ssh_sg" {
+  count = "${length(var.allow_ssh_to_runner_instance_sg)}"
+
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = "${var.allow_ssh_to_runner_instance_sg[count.index]}"
+
+  security_group_id = "${aws_security_group.runner.id}"
+}
+
+resource "aws_security_group_rule" "runner_ssh_cidr" {
+  count = "${length(var.allow_ssh_to_runner_instance_cidr)}"
+
+  type        = "ingress"
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = ["${var.allow_ssh_to_runner_instance_cidr[count.index]}"]
+
+  security_group_id = "${aws_security_group.runner.id}"
 }
 
 # sg of the runner created instances that run the ci-jobs
